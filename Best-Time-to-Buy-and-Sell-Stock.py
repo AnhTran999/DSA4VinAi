f5a3d1ec9a8c135f190cd557066486ea09737291
AnhTran999 <107304598+AnhTran999@users.noreply.github.com>
2025-07-08 15:03:08 +0700

    [121] [Time Beats: 5.02%] [Memory Beats: 8.47%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.py
+++ Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,9 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        hold = -prices[0]
-        cash = 0 
-        for price in prices[1:]: 
-            hold = max(hold, -price)
-            cash = max(cash, price + hold)
-        return cash
+        n = len(prices)
+        dp = [[0,0] for _ in range(n)] #create dp 2d matrix
+        dp[0][0] = 0 # do nothing in day 1 
+        dp[0][1] = -prices[0] # if buy stock on day 1
+        for i in range(1,n): 
+            dp[i][0] = max(dp[i-1][0] , dp[i-1][1] + prices[i])
+            dp[i][1] = max(dp[i-1][1], -prices[i] )
+        return dp[n-1][0] 
         
